d73af7c6fc075fff3409cb4b9c7fba9d4c4045c8
Caroline Reid <carolinehreid@gmail.com>
2017-12-06 16:16:08 +0000

    adds API, makeRequest and requestComplete functions

--- public/app.js
+++ public/app.js
@@ -1,5 +1,21 @@
 var app = function(){
+	var url = 'https://api.punkapi.com/v2/beers';
+	makeRequest(url, requestComplete);
+}
 
+var makeRequest = function(url, callback){
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.addEventListener('load', callback);
+	request.send();
+}
+
+var requestComplete = function(){
+	if(this.status !== 200) return;
+	var jsonString = this.responseText;
+	console.log(jsonString);
+	var beers = JSON.parse(jsonString);
+	var beer = beers[0];
 }
 
 window.addEventListener('load', app);
